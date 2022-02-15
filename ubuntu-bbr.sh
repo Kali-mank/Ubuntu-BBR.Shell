82e7042717f8e3934fd538e3966db92dcacea4bc
Mank <65970338+Kali-mank@users.noreply.github.com>
2022-02-15 19:56:19 +0800

    Update ubuntu-bbr.sh

--- ubuntu-bbr.sh
+++ ubuntu-bbr.sh
@@ -1,17 +1,45 @@
 #!/bin/bash
 echo "###!!!    Author:Mank Email:admin@mank.top"
-echo "###!!!    本脚本为Ubuntu加速脚本 Ubuntu 20.04+"
-echo "###!!!    懒得写系统判断 自己看清楚再运行"
+echo "###!!!    本脚本为Ubuntu加速脚本 Ubuntu 20.04-ubuntu 18.04"
+echo "###!!!    自己看清楚再运行"
 
 
 
 echo "Do you wish to Run this shell?"
 select yn in "Yes" "No"; do
-	    case $yn in
-		            Yes ) echo "Loading" ; break;;
-			            No ) exit;;
-				        esac
-				done
+echo "Do you wish to Run this shell?"
+select yn in "Yes" "No"; do
+            case $yn in
+                            Yes ) echo "Loading..。.." ; break;;
+                                    No ) exit;;
+                                        esac
+                                done
+
+echo "检测系统中"
+
+a=`uname  -a`
+
+b="Darwin"
+c="centos"
+d="ubuntu"
+
+if [[ $a =~ $b ]];then
+    echo "mac不支持" 
+    echo "已退出"
+    exit 8
+    echo "未退出"
+elif [[ $a =~ $c ]];then
+    echo "centos不支持"
+    echo "已退出"
+    exit 8
+    echo "未退出"
+elif [[ $a =~ $d ]];then
+    echo "ubuntu"
+else
+    echo $a
+fi
+
+
 
 
 echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
@@ -19,3 +47,4 @@ echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
 sysctl -p
 lsmod | grep bbr
 echo done!
+                                  
